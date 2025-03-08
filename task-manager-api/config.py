bbd6bd6af3814fcf73a2c822972eec757bf0237a
istamosh <63596969+istamosh@users.noreply.github.com>
2025-03-08 14:41:47 +0700

    update alfian 2

--- task-manager-api/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    DEBUG = os.getenv('DEBUG', 'False') == 'True'
-    TESTING = os.getenv('TESTING', 'False') == 'True'
-    DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///tasks.db')
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key_here')
